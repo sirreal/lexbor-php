75093f61dd5b35649a2c8b735409f55f437ab03f
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-23 13:45:27 +0200

    Port UTF-8 single encode tests

--- tests/Encoding/Utf8Test.php
+++ tests/Encoding/Utf8Test.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Lexbor\Tests\Encoding;
 
+use Lexbor\Core\LexborException;
+use Lexbor\Core\Status;
 use Lexbor\Encoding\Utf8;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
@@ -61,5 +63,38 @@ final class Utf8Test extends TestCase
         self::assertSame($utf8Expected, Utf8::skipUtf8Bom($utf8));
         self::assertSame($utf16BeExpected, Utf8::skipUtf16BeBom($utf16Be));
         self::assertSame($utf16LeExpected, Utf8::skipUtf16LeBom($utf16Le));
+    }
+
+    /**
+     * @return iterable<string, array{int, string}>
+     */
+    public static function upstreamUtf8EncodeProvider(): iterable
+    {
+        yield 'single/utf-8.c encode #1 one byte upper edge' => [0x7F, "\x7F"];
+        yield 'single/utf-8.c encode #2 two byte lower edge' => [0x80, "\xC2\x80"];
+        yield 'single/utf-8.c encode #3 two byte upper edge' => [0x07FF, "\xDF\xBF"];
+        yield 'single/utf-8.c encode #4 three byte lower edge' => [0x0800, "\xE0\xA0\x80"];
+        yield 'single/utf-8.c encode #5 three byte upper edge' => [0xFFFF, "\xEF\xBF\xBF"];
+        yield 'single/utf-8.c encode #6 four byte lower edge' => [0x10000, "\xF0\x90\x80\x80"];
+        yield 'single/utf-8.c encode #7 four byte upper edge' => [0x10FFFF, "\xF4\x8F\xBF\xBF"];
+    }
+
+    #[DataProvider('upstreamUtf8EncodeProvider')]
+    public function testUpstreamUtf8SingleEncode(int $codePoint, string $expected): void
+    {
+        self::assertSame($expected, Utf8::encodeCodePoint($codePoint));
+        self::assertSame(strlen($expected), strlen(Utf8::encodeCodePoint($codePoint)));
+    }
+
+    public function testUpstreamUtf8SingleEncodeRejectsOutOfRangeCodePoint(): void
+    {
+        try {
+            Utf8::encodeCodePoint(0x110000);
+        } catch (LexborException $exception) {
+            self::assertSame(Status::ErrorUnexpectedData, $exception->status);
+            return;
+        }
+
+        self::fail('Expected LexborException for out-of-range UTF-8 code point.');
     }
 }
