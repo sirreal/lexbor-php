65ae4edd585c481928993427ee944c7dc43bc124
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-23 15:16:18 +0200

    Port Windows-1258 encoding tests

---
+++ src/Encoding/Windows1258.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lexbor\Encoding;
+
+final class Windows1258
+{
+    private const string LABEL = 'Windows-1258';
+    private const string TABLE_KEY = 'windows_1258';
+
+    /**
+     * Lexbor's lxb_encoding_single_index_windows_1258 table for bytes 0x80-0xFF.
+     *
+     * @var list<int>
+     */
+    private const array DECODE_INDEX = [
+        0x20AC, 0x0081, 0x201A, 0x0192, 0x201E, 0x2026, 0x2020, 0x2021,
+        0x02C6, 0x2030, 0x008A, 0x2039, 0x0152, 0x008D, 0x008E, 0x008F,
+        0x0090, 0x2018, 0x2019, 0x201C, 0x201D, 0x2022, 0x2013, 0x2014,
+        0x02DC, 0x2122, 0x009A, 0x203A, 0x0153, 0x009D, 0x009E, 0x0178,
+        0x00A0, 0x00A1, 0x00A2, 0x00A3, 0x00A4, 0x00A5, 0x00A6, 0x00A7,
+        0x00A8, 0x00A9, 0x00AA, 0x00AB, 0x00AC, 0x00AD, 0x00AE, 0x00AF,
+        0x00B0, 0x00B1, 0x00B2, 0x00B3, 0x00B4, 0x00B5, 0x00B6, 0x00B7,
+        0x00B8, 0x00B9, 0x00BA, 0x00BB, 0x00BC, 0x00BD, 0x00BE, 0x00BF,
+        0x00C0, 0x00C1, 0x00C2, 0x0102, 0x00C4, 0x00C5, 0x00C6, 0x00C7,
+        0x00C8, 0x00C9, 0x00CA, 0x00CB, 0x0300, 0x00CD, 0x00CE, 0x00CF,
+        0x0110, 0x00D1, 0x0309, 0x00D3, 0x00D4, 0x01A0, 0x00D6, 0x00D7,
+        0x00D8, 0x00D9, 0x00DA, 0x00DB, 0x00DC, 0x01AF, 0x0303, 0x00DF,
+        0x00E0, 0x00E1, 0x00E2, 0x0103, 0x00E4, 0x00E5, 0x00E6, 0x00E7,
+        0x00E8, 0x00E9, 0x00EA, 0x00EB, 0x0301, 0x00ED, 0x00EE, 0x00EF,
+        0x0111, 0x00F1, 0x0323, 0x00F3, 0x00F4, 0x01A1, 0x00F6, 0x00F7,
+        0x00F8, 0x00F9, 0x00FA, 0x00FB, 0x00FC, 0x01B0, 0x20AB, 0x00FF,
+    ];
+
+    /**
+     * @return list<int>
+     */
+    public static function decodeWithReplacement(string $data): array
+    {
+        return SingleByteEncoding::decodeWithReplacement($data, self::DECODE_INDEX, self::LABEL);
+    }
+
+    public static function decodeToBuffer(string $data, int $capacity, int $offset = 0): DecodeResult
+    {
+        return SingleByteEncoding::decodeToBuffer($data, $capacity, $offset, self::DECODE_INDEX, self::LABEL);
+    }
+
+    public static function encodeCodePoint(int $codePoint): string
+    {
+        return SingleByteEncoding::encodeCodePoint($codePoint, self::DECODE_INDEX, self::LABEL, self::TABLE_KEY);
+    }
+
+    public static function encodeCodePointWithCapacity(int $codePoint, int $capacity): EncodeResult
+    {
+        return SingleByteEncoding::encodeCodePointWithCapacity(
+            $codePoint,
+            $capacity,
+            self::DECODE_INDEX,
+            self::LABEL,
+            self::TABLE_KEY,
+        );
+    }
+
+    /**
+     * @param list<int> $codePoints
+     */
+    public static function encodeCodePointsToBuffer(array $codePoints, int $capacity): BufferEncodeResult
+    {
+        return SingleByteEncoding::encodeCodePointsToBuffer(
+            $codePoints,
+            $capacity,
+            self::DECODE_INDEX,
+            self::LABEL,
+            self::TABLE_KEY,
+        );
+    }
+
+    public static function decodeByte(int $byte): int
+    {
+        return SingleByteEncoding::decodeByte($byte, self::DECODE_INDEX, self::LABEL);
+    }
+
+    public static function encodeByte(int $codePoint): ?int
+    {
+        return SingleByteEncoding::encodeByte($codePoint, self::DECODE_INDEX, self::TABLE_KEY);
+    }
+}
