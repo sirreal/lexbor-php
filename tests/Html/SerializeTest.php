a56418a9674379c269d6508ff37442c4eacf46ad
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-28 19:52:15 +0200

    Port test3 tag name punctuation rows

--- tests/Html/SerializeTest.php
+++ tests/Html/SerializeTest.php
@@ -4309,6 +4309,36 @@ final class SerializeTest extends TestCase
     }
 
     /**
+     * @return iterable<string, array{string, int, string, list<string>, list<list<mixed>>, list<array{code: string, line: int, col: int}>}>
+     */
+    public static function html5libTest3TagNamePunctuationSlashFixtureProvider(): iterable
+    {
+        foreach ([
+            1497 => ['<a!>', '<a!>', [['StartTag', 'a!', []]], []],
+            1498 => ['<a">', '<a">', [['StartTag', 'a"', []]], []],
+            1499 => ['<a&>', '<a&>', [['StartTag', 'a&', []]], []],
+            1500 => ["<a'>", "<a'>", [['StartTag', "a'", []]], []],
+            1501 => ['<a->', '<a->', [['StartTag', 'a-', []]], []],
+            1502 => ['<a.>', '<a.>', [['StartTag', 'a.', []]], []],
+            1503 => ['<a/>', '<a/>', [['StartTag', 'a', [], true]], []],
+        ] as $testIndex => [$description, $html, $expectedOutput, $errors]) {
+            $expectedErrors = array_map(
+                static fn (array $error): array => ['code' => $error[0], 'line' => $error[1], 'col' => $error[2]],
+                $errors,
+            );
+
+            yield "test3.test $testIndex $description tag-name punctuation/slash exact fixture row" => [
+                $html,
+                $testIndex,
+                $description,
+                [],
+                $expectedOutput,
+                $expectedErrors,
+            ];
+        }
+    }
+
+    /**
      * @return iterable<string, array{string, int, string, list<string>, list<list<string>>, list<array{code: string, line: int, col: int}>}>
      */
     public static function html5libTest3CommentStartFixtureProvider(): iterable
@@ -14054,6 +14084,36 @@ final class SerializeTest extends TestCase
      */
     #[DataProvider('html5libTest3AttributeNameContinuationAsciiNonBmpFixtureProvider')]
     public function testHtml5libTest3AttributeNameContinuationAsciiNonBmpFixtureRows(
+        string $html,
+        int $testIndex,
+        string $description,
+        array $initialStates,
+        array $expectedOutput,
+        array $expectedErrors,
+    ): void
+    {
+        $contents = file_get_contents(dirname(__DIR__, 2) . '/upstream/lexbor/test/files/lexbor/html/html5lib_tokenizer/test3.test');
+        self::assertIsString($contents);
+
+        $data = json_decode($contents, true, flags: JSON_THROW_ON_ERROR);
+        self::assertIsArray($data);
+
+        $fixture = $data['tests'][$testIndex] ?? null;
+        self::assertIsArray($fixture);
+        self::assertSame($description, $fixture['description']);
+        self::assertSame($initialStates, $fixture['initialStates'] ?? []);
+        self::assertSame($html, $fixture['input']);
+        self::assertSame($expectedOutput, $fixture['output']);
+        self::assertSame($expectedErrors, $fixture['errors'] ?? []);
+    }
+
+    /**
+     * @param list<string> $initialStates
+     * @param list<list<mixed>> $expectedOutput
+     * @param list<array{code: string, line: int, col: int}> $expectedErrors
+     */
+    #[DataProvider('html5libTest3TagNamePunctuationSlashFixtureProvider')]
+    public function testHtml5libTest3TagNamePunctuationSlashFixtureRows(
         string $html,
         int $testIndex,
         string $description,
